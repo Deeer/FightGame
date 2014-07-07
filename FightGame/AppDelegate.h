08694bfa1a60329155cc630aa7a30e76419a6cbb
zjsruxxxy3 <zjsruxxxy3@host20c9d0dbf66b.workgroup>
2014-07-07 22:44:26 +0800

    Initial Commit

---
+++ FightGame/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  FightGame
+//
+//  Created by zjsruxxxy3 on 14-7-7.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ FightGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FightGame
+//
+//  Created by zjsruxxxy3 on 14-7-7.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
